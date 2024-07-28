09c31b56bcf2886c9af626e28436a0225899a37d
Albert Diones <adiones@addteam.org>
2024-07-28 18:59:14 +0800

    sleep func

--- index.ts
+++ index.ts
@@ -122,8 +122,20 @@ class Repeater {
     }
 }
 
-if (Bun) {
-    Repeater.sleep = (interval: number) => Bun.sleep(interval);
+const sleepFunc = (interval: number) => new Promise<void>(resolve => {
+    setTimeout(resolve, interval)
+})
+
+if (global.Bun) {
+    /* Use normal sleep func if bun version not more than or equal 1 */
+    if (Bun.semver.satisfies(Bun.version, "(x.y.z | x >= 1)")) {
+        Repeater.sleep = (interval: number) => Bun.sleep(interval);
+    } else {
+        Repeater.sleep = sleepFunc
+    }
+} else {
+    /* Node */
+    Repeater.sleep = sleepFunc
 }
 
 export default Repeater;
