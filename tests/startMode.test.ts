530493179680bdd7bb29ec1d027b752768417f43
Albert Diones <adiones@addteam.org>
2025-06-29 18:53:53 +0800

    waitFirst mode test

---
+++ tests/startMode.test.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@jest/globals'
+
+import Repeater from '..';
+
+test(
+    'wait first',
+    async () => {
+        const timestamps: number[] = [];
+
+        const repeater = new Repeater(
+            () => {
+                const now = Date.now();
+                console.log(now);
+                timestamps.push(now);
+                return Promise.resolve();
+            }
+        );
+        repeater.continuous(500,null,{startMode: Repeater.startMode.waitFirst});
+
+        await Bun.sleep(400);
+
+        expect(timestamps.length).toBe(0);
+
+        await Bun.sleep(100);
+        expect(timestamps.length).toBe(1);
+        repeater.stop();
+        
+        expect(timestamps.length).toBe(1);
+
+        await Bun.sleep(500);
+        
+        expect(timestamps.length).toBe(1);
+    }
+);
