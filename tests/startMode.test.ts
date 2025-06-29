176a0111e9c7c0f1dd743885627c9cf1f72f99e9
Albert Diones <adiones@addteam.org>
2025-06-29 18:55:45 +0800

    action first startMode test

--- tests/startMode.test.ts
+++ tests/startMode.test.ts
@@ -32,3 +32,36 @@ test(
         expect(timestamps.length).toBe(1);
     }
 );
+
+
+
+test(
+    'action first',
+    async () => {
+        const timestamps: number[] = [];
+
+        const repeater = new Repeater(
+            () => {
+                const now = Date.now();
+                console.log(now);
+                timestamps.push(now);
+                return Promise.resolve();
+            }
+        );
+        repeater.continuous(500,null,{startMode: Repeater.startMode.actionFirst});
+
+        await Bun.sleep(400);
+
+        expect(timestamps.length).toBe(1);
+
+        await Bun.sleep(100);
+        expect(timestamps.length).toBe(2);
+        repeater.stop();
+        
+        expect(timestamps.length).toBe(2);
+
+        await Bun.sleep(500);
+        
+        expect(timestamps.length).toBe(2);
+    }
+);
